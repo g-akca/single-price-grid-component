a7108dac443549dbc0ef3d05115c1775015d67d0
g-akca <gok.akca66@gmail.com>
2026-06-16 20:54:58 +0300

    Add styling to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import MainSection from "./components/MainSection";
 
 function App() {
   return (
-    <div>
+    <div className="min-h-screen bg-teal-100 py-6.25 px-8 text-base leading-base text-gray-500">
       <MainSection />
     </div>
   )
