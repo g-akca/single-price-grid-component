b49f55a91dae8543af5bf3d999b7f9393e3652f8
g-akca <gok.akca66@gmail.com>
2026-06-17 10:05:44 +0300

    Center items on App, add styling to MainSection

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import MainSection from "./components/MainSection";
 
 function App() {
   return (
-    <div className="min-h-screen bg-teal-100 py-6.25 px-8 text-base leading-base text-gray-500">
+    <div className="min-h-screen bg-teal-100 py-6.25 px-8 text-base leading-base text-gray-500 flex flex-col justify-center items-center">
       <MainSection />
     </div>
   )
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -3,7 +3,7 @@ import SubscriptionSection from "./SubscriptionSection";
 
 function MainSection() {
   return (
-    <main>
+    <main className="rounded-lg shadow-[0_15px_30px_rgba(0,81,171,0.15)] w-full max-w-158.75 overflow-hidden flex flex-col">
       <HeroSection />
       <SubscriptionSection />
     </main>
