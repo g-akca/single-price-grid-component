e93bb7e222c8431f69443bf0e09300fe4f5cb184
g-akca <gok.akca66@gmail.com>
2026-06-17 11:40:36 +0300

    Add content and styling to SubscriptionWhyUs

--- src/components/SubscriptionWhyUs.jsx
+++ src/components/SubscriptionWhyUs.jsx
@@ -1,7 +1,18 @@
 function SubscriptionWhyUs() {
   return (
-    <>
-    </>
+    <section className="bg-teal-400 text-white py-6 px-9 flex flex-col gap-4 justify-between">
+      <h2 className="text-[18px] leading-[115%] font-bold tracking-[-0.25px]">Why Us</h2>
+
+      <ul className="opacity-75 text-[14px] leading-[145%]">
+        <li>Tutorials by industry experts</li>
+        <li>Peer & expert code review</li>
+        <li>Coding exercises</li>
+        <li>Access to our GitHub repos</li>
+        <li>Community forum</li>
+        <li>Flashcard decks</li>
+        <li>New videos every week</li>
+      </ul>
+    </section>
   )
 }
 
